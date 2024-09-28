1fd6f4e3876acbac068a4b00d4836f3cbe115757
A-Pujo <68434815+A-Pujo@users.noreply.github.com>
2024-09-28 21:19:37 +0800

    setting up vercel

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./database');
+const db = require('../database');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Ready!"))
 
 // login
 app.post('/login', async (req, res) => {
