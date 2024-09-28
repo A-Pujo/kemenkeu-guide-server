36418026754bbb7c600f1bf740d44bc691eb33f7
A-Pujo <68434815+A-Pujo@users.noreply.github.com>
2024-09-28 23:05:24 +0800

    re-setting netlify

--- functions/api/api.js
+++ functions/api/api.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const db = require('../../database');
 
-const app = express();
+const handler = async (event) => {
+  try {
+    const subject = event.queryStringParameters.name || 'World'
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
 
-app.get('/', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
-    res.json({ message: 'Hello from Netlify!' });
-});
-
-module.exports.handler = serverless(app);
+module.exports = { handler }
